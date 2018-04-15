3cac97c211835c5fca63aef7d8361240a4db6e25
OlafAndreas <olaf.ovrum@gmail.com>
2018-04-15 22:52:48 +0200

    Added a simple flow for receiving credentials and output a token.

--- main.go
+++ main.go
@@ -1,5 +1,56 @@
 package main
 
+import (
+  "fmt"
+  "log"
+  "net/http"
+  "encoding/json"
+)
+
 func main() {
-  
+
+	http.HandleFunc("/auth", auth)
+	logError(http.ListenAndServe(":8080", nil))
+  fmt.Println("Listening on port 8080.")
+}
+
+func logError(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+type credentails struct {
+  Username string
+  Password string
+}
+
+func auth(w http.ResponseWriter, r *http.Request) {
+
+  // Decode request body into credentials
+
+  cred := credentails{}
+
+  decoder := json.NewDecoder(r.Body)
+
+  err := decoder.Decode(&cred)
+  if err != nil {
+    logError(err)
+    fmt.Fprintf(w, "Failed to decode body.")
+  }
+
+  defer r.Body.Close()
+  w.Header().Set("Content-Type", "application/json")
+
+  token := make(map[string]string)
+  token["token"] = "SomeLongHashesTokenToUseInBearer"
+
+  json, err := json.Marshal(token)
+  if err != nil {
+    logError(err)
+    fmt.Fprintf(w, "Failed to generate token")
+  }
+
+  fmt.Fprintf(w, string(json))
+  fmt.Println(string(json))
 }
